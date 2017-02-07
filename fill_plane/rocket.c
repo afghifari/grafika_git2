588e497c79110af2278ae6cdf71f9383ec5103db
rmxhaha <code000123@gmail.com>
2017-02-07 17:46:41 +0700

    fix hardcode cannon coordinate

---
+++ fill_plane/rocket.c
@@ -0,0 +1,7 @@
+#include "rocket.h"
+
+void buildRocket(int x, int y, color c){
+}
+
+void fillRocket(int x, int y, color fin_color, color warhead_color, color body_color){
+}
---
+++ fill_plane/rocket.h
@@ -0,0 +1,12 @@
+#ifndef ROCKET_H
+#define ROCKET_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include "cannon.h"
+
+void buildRocket(int x, int y, color c);
+
+void fillRocket(int x, int y, color fin_color, color warhead_color, color body_color);
+
+#endif
