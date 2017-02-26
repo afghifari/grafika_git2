55097877132717cf1e9de5587b4e764c7bf44ed9
afghifari <alhudaghifari@gmail.com>
2017-02-26 21:09:01 +0700

    get ready to tugas6

---
+++ 6tugas/Print.h
@@ -0,0 +1,19 @@
+#ifndef PRINT_H
+#define PRINT_H
+
+#include "Point.h"
+#include "Color.h"
+
+class Print {
+	public :
+		Print();
+		~Print();
+
+		PrintBackground();
+
+
+	private :
+		Point H;
+
+};
+#endif
